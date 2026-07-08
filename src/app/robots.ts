87331da6744750488babf4749b9223f55704cb0a
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2026-07-08 18:32:09 +0530

    Some changes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,16 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "AhrefsBot",
+        disallow: "/",
+      },
+    ],
     sitemap: "https://traindekho.live/sitemap.xml",
   };
 }
