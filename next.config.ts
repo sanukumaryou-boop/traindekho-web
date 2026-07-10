52f50ff9a5e13e9998096ed5950f4645063385c5
Ankit kumar <sanukumaryou@gmail.com>
2026-07-10 21:11:45 +0530

    Update Next.js configuration to set basePath and assetPrefix for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  basePath: isProd ? '/web' : '',
+  assetPrefix: isProd ? '/web' : '',
   compress: true,
   poweredByHeader: false,
   async headers() {
